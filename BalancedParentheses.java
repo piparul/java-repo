2d196a7805d94c73bdba5afe7b25dc0dbe9f9f83
husky-parul <promptparul@gmail.com>
2017-01-29 21:03:00 -0500

    BalancedParentheses.java

--- BalancedParentheses.java
+++ BalancedParentheses.java
@@ -12,14 +12,7 @@ public class BalancedParentheses {
     public static boolean isBalanced(String expression) {
         Stack<Character> stack1= new Stack<Character>();
        List<Character> openingSymbol=new ArrayList<Character>();
-       openingSymbol.add('{');
-       openingSymbol.add('(');
-       openingSymbol.add('[');
-       
-       List<Character> closingSymbol=new ArrayList<Character>();
-       closingSymbol.add('}');
-       closingSymbol.add(')');
-       closingSymbol.add(']');
+
        
        Map<Character,Character> map=new HashMap<Character, Character>();
        map.put('(', ')');
@@ -32,9 +25,9 @@ public class BalancedParentheses {
         boolean flag=true;
         for(int i=0;i<=size;i++){
         	symbol=expression.charAt(i);
-        	if (openingSymbol.contains(symbol))
+        	if (symbol =='[' || symbol == '(' || symbol=='{')
         		stack1.push(symbol);
-        	else if (closingSymbol.contains(symbol)){
+        	else if (stack1.size()>0 && (symbol ==']' || symbol == ')' || symbol=='}')){
         		match=stack1.pop();
         		if(map.get(match)!=symbol){
         			flag=false;
